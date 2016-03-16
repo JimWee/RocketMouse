4c4e9241b94151973bff7d22f062dafb63e5faa1
JianhuiWei <18817551783@163.com>
2016-03-16 16:56:53 +0800

    完成

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,11 +1,39 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class UIManager : MonoBehaviour {
 
 
+    public Animator StartBtnAnimator;
+    public Animator SettingBtnAnimator;
+    public Animator SettingDlgAnimator;
+    public Animator ContentPnlAnimator;
+    public Animator GearImgAnimator;
+
     public void StartGame()
     {
-        Application.LoadLevel("RocketMouse");
+        SceneManager.LoadScene("RocketMouse");
+    }
+
+    public void OpenSettings()
+    {
+        StartBtnAnimator.SetBool("isHidden", true);
+        SettingBtnAnimator.SetBool("isHidden", true);
+        SettingDlgAnimator.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings()
+    {
+        StartBtnAnimator.SetBool("isHidden", false);
+        SettingBtnAnimator.SetBool("isHidden", false);
+        SettingDlgAnimator.SetBool("isHidden", true);
+    }
+
+    public void ToggleMenu()
+    {
+        bool isHidden = ContentPnlAnimator.GetBool("isHidden");
+        ContentPnlAnimator.SetBool("isHidden", !isHidden);
+        GearImgAnimator.SetBool("isHidden", !isHidden);
     }
 }
